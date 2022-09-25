d1b435f9557b7af56b89251a0c22a0ac466d49d9
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2022-09-25 20:27:22 +0300

    Fix the issues

---
+++ lab1_task15/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "./strucure.c"
+
+// Assuming that words are not longer than 128 characters
+#define MAX_WORD_LENGTH 128
+
+Node* readFile(char const *fileName) {
+  FILE *fileStream = fopen(fileName, "r");
+
+  if (!fileStream) {
+    printf("Could not open the file %s", fileName);
+    exit(0);
+  }
+
+  const int wordSize = sizeof(char) * MAX_WORD_LENGTH;
+  Node* wordsArray = listCreate();
+  char* buffer = malloc(wordSize);
+  char* currentWord = malloc(wordSize);
+
+  while (fscanf(fileStream, "%s", buffer) != EOF) {
+    memcpy(currentWord, buffer, wordSize);
+    listPush(wordsArray, currentWord);
+    currentWord = malloc(wordSize);
+  }
+
+  fclose(fileStream);
+
+  return wordsArray;
+}
+
+Node* mergeArrays(Node* firstArray, Node* secondArray) {
+  Node* mergedArray = listCreate();
+
+  for (Node* node = firstArray; node != NULL; node = node->next) {
+    listPush(mergedArray, node->value);
+  }
+
+  for (Node* node = secondArray; node != NULL; node = node->next) {
+    listPush(mergedArray, node->value);
+  }
+
+  return mergedArray;
+}
+
+void insertionSort(Node* array) {
+  char* word;
+  int i = 1;
+  int j;
+
+  for (Node *node = array->next; node != NULL; node = node->next) {
+    word = node->value;
+    j = i - 1;
+
+    while (j >= 0 && strcmp(listAt(array, j)->value, word) > 0) {
+      listAt(array, j + 1)->value = listAt(array, j)->value;
+      j -= 1;
+    }
+
+    listAt(array, j + 1)->value = word;
+    i += 1;
+  }
+}
---
+++ lab1_task15/strucure.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct Node {
+	char* value;
+	struct Node* next;
+} Node;
+
+Node* listCreate() {
+	Node* head = malloc(sizeof(Node));
+
+	head->value = NULL;
+	head->next = NULL;
+
+	return head;
+}
+
+Node* listLast(Node* head) {
+	Node* currentNode = head;
+
+	while (currentNode->next != NULL) {
+		currentNode = currentNode->next;
+	}
+
+	return currentNode;
+}
+
+Node* listAt(Node* head, int index) {
+	Node *currentNode = head;
+	int currentIndex = 0;
+
+	while (currentIndex != index) {
+		if (currentNode->next == NULL)
+			assert(0);
+
+		currentNode = currentNode->next;
+		currentIndex += 1;
+	}
+
+	return currentNode;
+}
+
+void listPush(Node* head, char* value) {
+	Node* lastNode = listLast(head);
+
+	if (lastNode->value == NULL) {
+		lastNode->value = value;
+	} else {
+		Node* node = malloc(sizeof(Node));
+
+		node->value = value;
+		lastNode->next = node;
+	}
+}
+
+void listForEach(Node* head, void (*callBack)(char*)) {
+	for (Node* node = head; node != NULL; node = node->next) {
+		callBack(node->value);
+	}
+}
