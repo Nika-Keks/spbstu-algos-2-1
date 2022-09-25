bbb0b4ba861ed05546100edbf3e7377ef9c83bd1
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2022-09-25 22:32:28 +0300

    Fix filename

--- lab1_task15/main.c
+++ lab1_task15/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "./strucure.c"
+#include "./structure.c"
 
 // Assuming that words are not longer than 128 characters
 #define MAX_WORD_LENGTH 128
