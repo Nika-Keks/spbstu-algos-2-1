c66643f9ce977e3c32f25d85c579e8c149158d23
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2022-10-21 10:38:52 +0300

    Fix include

---
+++ lab4_task1/fs.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int *readNumbers(char *filename);
--- lab4_task1/main.c
+++ lab4_task1/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include "./fs.c"
+#include "fs.h"
 
 int getNumberLength(int number) {
   if (number == 0)
