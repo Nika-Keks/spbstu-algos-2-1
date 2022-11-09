760f35ab2aa2023864f603ea00d026f3a640c66c
LuckyAm20 <aptukovm@yandex.ru>
2022-11-09 23:45:53 +0300

    Aptukov_laba_4_var_1

---
+++ laba_4/main.c
@@ -0,0 +1,12 @@
+#pragma warning(disable : 4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include "function.h"
+
+int main(void)
+{
+	calc("A.txt","B.txt");
+	printf("\n\n\n");
+	calc("C.txt", "D.txt");
+	
+}
