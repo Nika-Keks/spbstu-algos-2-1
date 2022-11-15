bde978ca47166a269700e4f2fd6b658c4e25d0ee
Maxim Kromachev <114993786+kromachev@users.noreply.github.com>
2022-11-15 22:58:15 +0300

    labs2

--- labs2/Source.c
+++ labs2/Source.c
@@ -9,7 +9,7 @@ int main(void) {
 	printf("Enter the number of data blocks: ");
 	int n;
 	//user enters the number of user requests
-	scanf_s("%d", &n);
+	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
 
 		Listptr tmp = (Listptr)malloc(sizeof(List));
@@ -23,23 +23,23 @@ int main(void) {
 		//for each memory block size in bytes
 		int m;
 		printf("Enter the number of memory blocks: ");
-		scanf_s("%d", &m);
+		scanf("%d", &m);
 		printf("Enter blocks of memory: ");
 		for (int j = 0; j < m; j++) {
 			int size;
 			void* memory;
-			scanf_s("%d", &size);
+			scanf("%d", &size);
 			memory = (void*)malloc(size);
 			Insert(tmp, j - 1, size, memory);
 		}
 		//the user enters the number of processes
 		printf("Enter the number of processes: ");
-		scanf_s("%d", &m);
+		scanf("%d", &m);
 		printf("Enter processes: ");
 		for (int j = 0; j < m; j++)
 		{
 			int value;//the complexity of each process expressed in bytes
-			scanf_s("%d", &value);
+			scanf("%d", &value);
 			bestfit(tmp, value);
 		}
 		if (tmp != NULL) {
